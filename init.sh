4b381a57570a5c28a22d657ac1abd026abfc2b76
Mike 'Fuzzy' Partin <fuzzy@creepypoo.com>
2020-01-05 14:02:29 -0800

    Initialization script

---
+++ init.sh
@@ -0,0 +1,32 @@
+set -a
+# Shell detection
+if test ! -z "${ZSH_NAME}"; then
+	THWAP_SHELL=zsh
+elif test ! -z "${BASH}"; then
+	THWAP_SHELL=bash
+fi
+
+# Some base utility detection
+if test -z "${THWAP_CMD_FETCH}"; then
+	if test ! -z "$(which wget 2>/dev/null)"; then
+		THWAP_CMD_FETCH="wget -qO-"
+	elif test ! -z "$(which curl 2>/dev/null)"; then
+		THWAP_CMD_FETCH="curl -q"
+	elif test ! -z "$(which fetch 2>/dev/null)"; then
+		THWAP_CMD_FETCH="fetch -q"
+	elif test ! -z "$(which ftp 2>/dev/null)"; then
+		THWAP_CMD_FETCH="ftp -q"
+	fi
+fi
+
+test -e ${HOME}/.profile.d/config.sh && source ${HOME}/.profile.d/config.sh
+set +a
+
+test -e ${HOME}/.profile.d/libthwap.sh && source ${HOME}/.profile.d/libthwap.sh
+t_chkdir ${THWAP_BINDIR}
+add2path ${THWAP_BINDIR}
+
+for fn in ${HOME}/.profile.d/???-*.sh; do
+	source ${fn}
+done
+
