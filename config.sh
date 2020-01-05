9c48d2cb33816c6019b3a7b905c35d6b974057b3
Mike 'Fuzzy' Partin <fuzzy@creepypoo.com>
2020-01-05 14:03:02 -0800

    added the config file and library of utility functions

---
+++ config.sh
@@ -0,0 +1,67 @@
+##############################################################################
+## Utility configuration knobs
+##############################################################################
+
+# This will reference our config file. This
+# is mainly for the management functions. You
+# shouldn't need to change this
+THWAP_CONFIG=${HOME}/.profile.d/config.sh
+
+# This is where all downloaded binaries will be dropped
+THWAP_BINDIR=${HOME}/.local/bin
+
+# This is where the management functions will
+# keep backups of configs and logs.
+THWAP_BKUP=${HOME}/.profile.d/.bkups
+
+# This sets the location of the ssh-agent env
+# settings file.
+THWAP_SSH_ENV=${HOME}/.ssh/ssh.env
+
+# This sets the identity of the main SSH pub key
+THWAP_SSH_IDENT=fuzzy@kerplop
+
+# Should we create keys if one is not present
+THWAP_SSH_KEY_CREATE="true"
+
+# And if we do, what bitrate should it have?
+THWAP_SSH_KEY_SIZE=4096
+
+##############################################################################
+## Support layers configuration knobs
+##############################################################################
+
+##### GoLang knobs
+
+# If you wish to use the Golang support in the
+# thwap utility set. Then set this to true
+THWAP_GOLANG="true"
+
+# This sets the location of the golang directories
+THWAP_GOLANG_BASE="${HOME}/.local/golang"
+
+# You can set the version of Go to be used, or
+# use the special values: latest or system.
+# Using 'system' will only setup your $GOPATH
+# locally, and use the system $GOROOT.
+THWAP_GOLANG_VERSION="latest"
+
+##### Starship prompt
+THWAP_STARSHIP="true"
+THWAP_STARSHIP_VERSION="0.32.2"
+
+##### Vlang knobs
+
+# Setting this to true will turn on the local vlang
+# build and support.
+THWAP_VLANG="false"
+
+##### Vim knobs
+
+# If you wish to use the Vim support set this
+THWAP_VIMRT="false"
+
+##### Emacs knobs
+
+# Also with emacs
+THWAP_EMACSRT="false"
---
+++ libthwap.sh
@@ -0,0 +1,100 @@
+export TERM_COLS=$(tput cols)
+
+tc_black="\033[30m"
+tb_black="\033[1;30m"
+tc_red="\033[31m"
+tb_red="\033[1;31m"
+tc_green="\033[32m"
+tb_green="\033[1;32m"
+tc_yellow="\033[33m"
+tb_yellow="\033[1;33m"
+tc_blue="\033[34m "
+tb_blue="\033[1;34m "
+tc_purple="\033[35m"
+tb_purple="\033[1;35m"
+tc_cyan="\033[36m"
+tb_cyan="\033[1;36m"
+tc_white="\033[37m"
+tb_white="\033[1;37m"
+tc_end="\033[0m"
+
+t_colmap() {
+	for i in 0 1 2 3 4 5 6 7; do
+		printf "3${i} = \033[3${i}mTEST\033[0m \033[1;3${i}mTEST\033[0m\n"
+	done
+}
+
+t_info() {
+	printf "[ ${tb_green}OK${tc_end} ] ${*}\n"
+}
+
+t_warn() {
+	printf "[${tb_yellow}WARN${tc_end}] ${*}\n"
+}
+
+t_debug() {
+	printf "[${tb_cyan}DEBG${tc_end}] ${*}\n"
+}
+
+t_error() {
+	printf "[${tb_red}FAIL${tc_end}] ${*}\n"
+}
+
+t_status_exec() {
+	r_true="[ ${tb_green}OK${tc_end} ]"
+	r_false="[${tb_red}FAIL${tc_end}]"
+	printf "[    ] ${1}\r"
+	/bin/sh -c "${2} 1>/tmp/th_exec.log 2>/tmp/th_exec.err"
+	test ${?} -eq 0 && printf "${r_true}\n" || printf "${r_false}\n"
+}
+
+t_trunc() {
+	trunc=$((${TERM_COLS} - $((${1}+4))))
+	echo "$(echo ${2}|cut -c 1-${trunc})..."
+}
+
+# This function will test for a directories existence
+# and create it if it does not already exist.
+t_chkdir() {
+	if test ! -d ${1}; then
+		t_info "Creating  : ${1}"
+		mkdir -p ${1}
+	fi
+}
+
+# This will fetch files, saving in the named location
+# Usage: t_fetch <URL> <OUTPUT>
+t_fetch() {
+	r_true="[ ${tb_green}OK${tc_end} ]"
+	r_false="[${tb_red}FAIL${tc_end}]"
+	printf "[    ] Fetching  : $(t_trunc 19 "${1}")\r"
+	/bin/sh -c "${THWAP_CMD_FETCH} ${1} >${2}"
+	test ${?} -eq 0 && printf "${r_true}\n" || printf "${r_false}\n"
+}
+
+# This function just wraps generating random strings
+# with openssl for convenience.
+trand() {
+	echo $(openssl rand -hex ${1})
+}
+
+# This function will backup the current on disk config
+# to the specified backup directory. Then it will dump
+# the current running config variables to the file.
+thwap_backup() {
+	case "${1}" in
+		(config)
+			BKUP_FNAME=$(basename ${THWAP_CONFIG}).$(trand 6)
+			t_info "Backing up config to: ${BKUP_FNAME}"
+			mv ${THWAP_CONFIG} ${THWAP_BKUP}/${BKUP_FNAME}
+			t_info "Dumping current config to: ${THWAP_CONFIG}"
+			#env|grep THWAP >${THWAP_CONFIG}
+			;;
+	esac
+}
+
+add2path() {
+	if test -z "$(echo ${PATH}|grep ${1})"; then
+		export PATH=${1}:${PATH}
+	fi
+}
